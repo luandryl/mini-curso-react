8fdf9736914de54215d9b331179cdbf5ed96b5ff
Luan <luan.andryl@gmail.com>
2019-09-20 14:56:42 -0300

    change: transform to class \w add components

--- src/App.js
+++ src/App.js
@@ -1,106 +1,94 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Icon from './assets/icons/Icon'
+import TaskCard from './components/task_card/TaskCard'
+import AppInput from './components/app_input/AppInput'
+import AppFooter from './components/app_footer/AppFooter'
 
-function App() {
-	return (
-		<div className="App center">
 
-			<div className="App--wrapper center">
+class App extends Component {
 
-				<div className="App--task-input center">
-					<h1> Todo App </h1>
-					<div className="task-input--actions center">
-						<input className="App__input" type="text" placeholder="type something and press add" /> 
-						<button className="App__button" > Add </button>
-					</div>
+	state = {
+		list: [{
+			id: 0,
+			name: 'make todo',
+			status: 0
+		},{
+			id: 1,
+			name: 'learn git',
+			status: 0
+		},{
+			id: 3,
+			name: 'learn js',
+			status: 0
+		},{
+			id: 4,
+			name: 'Learn DS in C',
+			status: 1
+		},{
+			id: 5,
+			name: 'Learn React',
+			status: 1
+		},{
+			id: 6,
+			name: 'Homework',
+			status: 2
+		}]
+	}
 
-				</div>
+	render () {
 
-				<div className="App--task-list center">
-					<div className="task--list-wrapper">
-						<h1> Todo </h1>
+		const { list } = this.state
+		
+		const todoTaskList = list.map(task => {
+			return (task.status === 0) && (<TaskCard task={task} />)
+		})
 
-						<div className="task--list">
+		const doingTaskList = list.map(task => {
+			return (task.status === 1) && (<TaskCard task={task} />)
+		})
 
-							<div className="task--card">
-								<span> Make Todo </span>
-									<div className="card--controls">
-									<Icon type="trash" />
-									<div className="div"> </div>
-									<Icon type="arrow" />
-								</div>
+		const doneTaskList = list.map(task => {
+			return (task.status === 2) && (<TaskCard task={task} />)
+		})
+		
+		return (
+			<div className="App center">
+	
+				<div className="App--wrapper center">
+
+					<AppInput />
+
+					<div className="App--task-list center">
+
+						<div className="task--list-wrapper">
+							<h1> Todo </h1>
+							<div className="task--list">
+								{todoTaskList}
 							</div>
-							<div className="task--card">
-								<span> Make Todo </span>
-									<div className="card--controls">
-									<Icon type="trash" />
-									<div className="div"> </div>
-									<Icon type="arrow" />
-								</div>
+						</div>
+
+						<div className="task--list-wrapper">
+							<h1> Doing </h1>
+							<div className="task--list">
+								{doingTaskList}
 							</div>
-							<div className="task--card">
-								<span> Make Todo </span>
-									<div className="card--controls">
-									<Icon type="trash" />
-									<div className="div"> </div>
-									<Icon type="arrow" />
-								</div>
+						</div>
+
+						<div className="task--list-wrapper">
+							<h1> Done </h1>
+							<div className="task--list">
+								{doneTaskList}
 							</div>
 						</div>
 
 					</div>
-					<div className="task--list-wrapper">
-						<h1> Doing </h1>
-
-						<div className="task--list">
-
-							<div className="task--card">
-								<span> Make Todo </span>
-									<div className="card--controls">
-									<Icon type="trash" />
-									<div className="div"> </div>
-									<Icon type="arrow" />
-								</div>
-							</div>
-							<div className="task--card">
-								<span> Make Todo </span>
-									<div className="card--controls">
-									<Icon type="trash" />
-									<div className="div"> </div>
-									<Icon type="arrow" />
-								</div>
-							</div>
-
-						</div>
-
-					</div>
-					<div className="task--list-wrapper">
-						<h1> Done </h1>
-
-						<div className="task--list">
-
-							<div className="task--card">
-								<span> Make Todo </span>
-									<div className="card--controls">
-									<Icon type="trash" />
-									<div className="div"> </div>
-									<Icon type="arrow" />
-								</div>
-							</div>
-
-							</div>
-
-					</div>
 				</div>
 
+				<AppFooter />
 
 			</div>
-			<div className="App--footer center">
-				<span>make with for </span> <Icon type="heart" className="space" /> <span>unectJr trainees</span>
-			</div> 
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
